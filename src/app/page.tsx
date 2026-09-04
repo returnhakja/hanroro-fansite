340c7c95f715be76d811fa0c0531919af03f1f40
도헌's <62533556+returnhakja@users.noreply.github.com>
2026-09-04 06:11:16 +0000

    Reapply "feat: 홈에 최근 발매 앨범 하이라이트 섹션 추가"
    This reverts commit bb02a5f18bb3c5446d18a283dd3c7a3be3c5b8c5.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Container } from "./_sections/common.styles";
 import HomeHeroQuick from "./_sections/HomeHeroQuick";
+import AlbumHighlightSection from "./_sections/AlbumHighlightSection";
 import BoardPreviewSection from "./_sections/BoardPreviewSection";
 import GalleryPreviewSection from "./_sections/GalleryPreviewSection";
 import SchedulePreviewSection from "./_sections/SchedulePreviewSection";
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <Container>
       <HomeHeroQuick />
+      <AlbumHighlightSection />
       {/* <NewReleaseSection /> */}
       <BoardPreviewSection />
       <GalleryPreviewSection />
